d7037ac0187b34c5abf8c31c1e68dbbf2f8eb0f8
Luan Teixeira <luanlts@outlook.com>
2020-10-19 00:35:22 -0300

    LUAN: Torneio teste InterFatecs [FINALIZADO]

---
+++ Python/1008.py
@@ -0,0 +1,6 @@
+nFunc = int(input())
+horas = int(input())
+salario = float(input()) * horas
+
+print("NUMBER = %i" %nFunc)
+print("SALARY = U$ %.2f" %salario)
---
+++ Python/1009.py
@@ -0,0 +1,7 @@
+nomeFunc = input()
+salario = float(input())
+vendas = float(input())
+
+salario = salario + (vendas * 0.15)
+
+print("TOTAL = %.2f" %salario)
